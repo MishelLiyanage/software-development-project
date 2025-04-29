aa9da9c8cd134ee82b4096cc851db35c7b04ac07
MishelLiyanage <mailtomishel@gmail.com>
2025-04-29 17:19:24 +0530

    Resolved some warnings

--- src/main/java/com/SDP/project/DTOs/response/OrderResponseDto.java
+++ src/main/java/com/SDP/project/DTOs/response/OrderResponseDto.java
@@ -1,11 +1,9 @@
 package com.SDP.project.DTOs.response;
 
-import com.SDP.project.models.Order;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 
 @Getter
 @Setter
