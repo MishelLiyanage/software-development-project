cbd6784cf287f8951b12ae80101ea4d9d3ad2d53
MishelLiyanage <mailtomishel@gmail.com>
2025-05-12 06:14:28 +0530

    Resolved some conflicts

--- src/main/java/com/SDP/project/Repository/DepartmentRepository.java
+++ src/main/java/com/SDP/project/Repository/DepartmentRepository.java
@@ -3,10 +3,9 @@ package com.SDP.project.Repository;
 import com.SDP.project.models.Department;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
     @EntityGraph(attributePaths = "employees")
     Department getDepartmentByName(String name);
